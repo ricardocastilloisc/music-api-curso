1b0ea0be4721785481ea58938d8f005fbcfca8d4
RicardoCastilloISC <ricardocastilloisc@gmail.com>
2019-06-24 08:42:45 -0500

    217. Crear el Repositorio JPA para la clase Album

---
+++ src/main/java/net/itinajero/repository/AlbumsRepository.java
@@ -0,0 +1,9 @@
+package net.itinajero.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import net.itinajero.api.entity.Album;
+
+public interface AlbumsRepository extends JpaRepository<Album, Integer> {
+
+}
